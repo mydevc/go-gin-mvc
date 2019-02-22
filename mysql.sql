1aab9e6d4590eb6d8c92b2181f0e4ba6c70f1f2d
fuxiaojun <fuxiaojun@youfa365.com>
2019-02-22 09:20:04 +0800

    数据库表测试用例

---
+++ mysql.sql
@@ -0,0 +1,44 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL COMMENT '用户id',
+  `name` varchar(20) NOT NULL,
+  `age` int(11) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id';
+COMMIT;
